04e168e0a990ddd6ee5ab31a39133251592ea411
powercreek <landanparker@gmail.com>
2022-12-31 20:18:59 -0600

    Removed state handling as we won't conflict.

--- infra1/main.tf
+++ infra1/main.tf
@@ -7,38 +7,18 @@ terraform {
   }
 }
 
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
-}
-
 provider "aws" {
   region  = "us-east-1"
   profile = "default"
 }
 
-resource "aws_s3_bucket" "my_terraform_state_bucket" {
-  bucket = "my-terraform-state-bucket-a-rwerwerwe"
-}
+# resource "aws_dynamodb_table" "dynamodb_table_lock_resource" {
+#   name         = "dynamo_state_lock_db"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-resource "aws_s3_bucket_acl" "bucket_acl_1" {
-  bucket = aws_s3_bucket.my_terraform_state_bucket.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_versioning" "bucket_versioning_1" {
-  bucket = aws_s3_bucket.my_terraform_state_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name         = "terraform-state-lock-dynamo"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
--- infra1/state.tf
+++
@@ -1,9 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket  = "my-terraform-state-bucket-a-rwerwerwe"
-#     dynamodb_table = "terraform-state-lock-dynamo"
-#     key     = "terraform.tfstate"
-#     region  = "us-east-1"
-#     encrypt = true    
-#   }
-# }
