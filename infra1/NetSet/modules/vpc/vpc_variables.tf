b0ec2da5b028a0070bb82e73c8a2ec39500c1107
powercreek <landanparker@gmail.com>
2023-01-01 09:59:42 -0600

    vpc_variables for subnet

---
+++ infra1/NetSet/modules/vpc/vpc_variables.tf
@@ -0,0 +1,36 @@
+variable "infra_env" {
+  type = string
+
+  description = "infrastructure environment"
+}
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+  type = string
+
+  description = "The IP range to use for the vpc"
+}
+
+variable "public_subnet_map" {
+    default = {
+        "us-east-1a" = 1
+        "us-east-1b" = 2
+        "us-east-1c" = 3
+    }
+
+    type = map(number)
+
+    description = "Map of AZ to a number that should be used for public subnets"
+}
+
+variable "private_subnet_map" {
+    default = {
+        "us-east-1d" = 4
+        "us-east-1e" = 5
+        "us-east-1f" = 6
+    }
+
+    type = map(number)
+
+    description = "Map of AZ to a number that should be used for private subnets"
+}
